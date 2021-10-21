5b4f21a852e414bd00ca35b408a2778a6cfb3dda
clecardona <clecardona@gmail.com>
2021-10-21 10:31:03 +0200

    polish header

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,15 +1,22 @@
 import { NavLink } from "react-router-dom";
 
-import logo from "assets/icns/football/flip.png";
+import logo from "assets/icns/bale-logo.png";
+import burger from "assets/icns/burger.png";
 
 export default function Header() {
   return (
     <header>
-      <NavLink to="/" className="home">
-        <img src={logo} alt="Home" />
-        <div className="home-shape" />
-      </NavLink>
-      <nav></nav>
+      <nav>
+        <NavLink to="/" className="home">
+          <img src={logo} alt="Home" />
+          <h1>
+            Foot<strong>X</strong>
+          </h1>
+        </NavLink>
+        <div className="burger">
+          <img src={burger} alt="." />
+        </div>
+      </nav>
     </header>
   );
 }
