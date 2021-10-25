02fc771c4b9bcc32ba3a11167044e5aaf1513d2e
clecardona <clecardona@gmail.com>
2021-10-25 09:02:27 +0200

    hero bg

--- src/components/VideoBG.jsx
+++ src/components/VideoBG.jsx
@@ -2,7 +2,7 @@ import video from "assets/video/bg.mp4";
 
 export default function VideoBG() {
   return (
-    <video autoPlay muted loop className="bg">
+    <video autoPlay muted loop playsInline className="bg">
       <source src={video} type="video/mp4" />
     </video>
   );
